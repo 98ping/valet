6954506e014a637136cd92fad6922c5c6221eaf6
98ping <67809373+98ping@users.noreply.github.com>
2025-10-18 22:44:13 -0700

    feat: make a stash dir and get ready for file prepping

--- src/main/kotlin/me/max/valet/artifact/UpdatableArtifact.kt
+++ src/main/kotlin/me/max/valet/artifact/UpdatableArtifact.kt
@@ -3,6 +3,7 @@ package me.max.valet.artifact
 import color_console_log.ColorConsoleContext.Companion.colorConsole
 import color_console_log.Colors
 import me.max.valet.interaction.GithubInteractionHandler
+import java.io.File
 import java.util.concurrent.ScheduledExecutorService
 
 /**
@@ -55,12 +56,47 @@ class UpdatableArtifact(
             }
         }
 
+        val directory = File(location)
+
+        if (!directory.exists())
+        {
+            colorConsole {
+                printLine {
+                    span(Colors.Red, "Unable to find this directory! Because of this, we are stopping the file scan.")
+                }
+            }
+
+            colorConsole {
+                printLine {
+                    span(Colors.Red, "To resolve this error, please ensure the directory exists and your project is loaded there.")
+                }
+            }
+
+            return
+        }
+
         val changedFiles = GithubInteractionHandler.fetchChangedFiles(owner, repo, branch, changes)
+        val historicStashDirectory = File(location, "stash")
+
+        // make a stash to ensure that no problems occur
+        if (!historicStashDirectory.exists())
+        {
+            historicStashDirectory.mkdirs()
+
+            colorConsole {
+                printLine {
+                    span(Colors.Green, "Creating stash directory to save changes")
+                }
+            }
+        }
+
         changedFiles.forEach { (path, content) ->
             if (content != null)
             {
                 println("$path: ${content.size} bytes")
-                // Write to disk here
+
+                val whereToWrite = File(location, path)
+
             } else
             {
                 println("$path: marked for deletion")
